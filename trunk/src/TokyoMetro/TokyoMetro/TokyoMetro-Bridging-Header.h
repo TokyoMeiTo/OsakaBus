923b87978067c844402288972133b2c252f86b09
limccn <limc@ohs-sys.com>
2014-09-24 20:33:24 +0900

    init commit
    init commit

---
+++ trunk/src/TokyoMetro/TokyoMetro/TokyoMetro-Bridging-Header.h
@@ -0,0 +1,25 @@
+//
+//  Use this file to import your target's public headers that you would like to expose to Swift.
+//
+
+// OKTOsakaKits
+#import "OKT.h"
+// ODBDatabase
+#import "ODB.h"
+// OAPAsyncParser
+#import "OAP.h"
+// ONWNetwork
+#import "ONW.h"
+
+#import "MstT01LineTable.h"
+#import "MstT02StationTable.h"
+#import "MstT03LineStationTable.h"
+#import "CmnT03StationGridTable.h"
+#import "InfT02TipsTable.h"
+#import "InfT03RescureTable.h"
+#import "InfT04StatKnowledgeTable.h"
+#import "InfT05LineKnowledgeTable.h"
+#import "UsrT01ArrivalAlarmTable.h"
+#import "UsrT02TrainAlarmTable.h"
+#import "LinT04RouteTable.h"
+#import "LinT06FareTable.h"
---
+++ trunk/src/TokyoMetro/TokyoMetro/ViewController.swift
@@ -0,0 +1,97 @@
+//
+//  ViewController.swift
+//  TokyoMetro
+//
+//  Created by limc on 2014/09/02.
+//  Copyright (c) 2014年 Okasan-Huada. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController,OAPAsyncParserDelegate {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        self.odbSample();
+    }
+    
+    
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+    func odbSample(){
+        var table = MstT01LineTable()
+        
+//       // table.select().item(aaaa,"").item().item().update();
+//        table.selectTop(1).array()[].key.item(MSTT01_LINE_ID, value: "111").update();
+     
+//        var rows:NSArray = table.excuteQuery("select LINE_ID from MSTT01_LINE where 1=1")
+//    
+//        for key in rows {
+//            
+//           key as MstT01LineTable
+//           var id:AnyObject = key.item(MSTT01_LINE_ID)
+//            
+//            println("\(id)")
+//        }
+
+        var tab = UsrT02TrainAlarmTable()
+        var row = tab.excuteQuery("select TRAI_ALAM_ID from USRT02_TRAIN_ALARM where 1=1")
+        for value in row{
+            value as UsrT02TrainAlarmTable
+            var id:AnyObject = value.item(USRT02_TRAIN_ALARM_TRAI_ALAM_ID)
+            
+            println("\(id)")
+        }
+        
+        
+        
+        
+        //table.lineId = "2800"
+        
+        //table.selectTop(3)
+        
+        
+        //for key in rows {
+        
+        ///    key as MstT01LineTable
+        //    var id:AnyObject = key.item(MSTT01_LINE_ID)
+        
+        //    println("\(id)")
+        //}
+//        var id:AnyObject = table.item(MSTT01_LINE_ID)
+//         println("\(id)")
+        
+    }
+    
+    
+    func jsonParserSample(){
+        var jsonparser = OAPAsyncJSONParser()
+        
+        // var err = NSError()
+        var url:NSURL = NSURL.URLWithString("http://www.weather.com.cn/data/sk/101010100.html")
+        
+        var data =
+        NSData.dataWithContentsOfURL(url, options: NSDataReadingOptions.DataReadingUncached, error: nil)
+        
+        var jsonStr=NSString(data:data,encoding:NSUTF8StringEncoding)
+        
+        var testJsonData=jsonStr.dataUsingEncoding(NSUTF8StringEncoding)
+        
+        jsonparser.objToBeParse = testJsonData
+        jsonparser.delegate = self
+        jsonparser.asyncParse();
+    }
+    
+    func parseDidFinished(parser:AnyObject){
+         println(parser.objParsed)
+    }
+
+}
+
