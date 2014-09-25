83e615e3c2d7cb4e50047359db543acad1abafdb
Rifleman <limc@ohs-sys.com>
2014-09-25 13:56:10 +0800

    item
    item

---
+++ trunk/src/TokyoMetro/TokyoMetro/Common/Extension/String+Convert.swift
@@ -0,0 +1,15 @@
+//
+//  File.swift
+//  TokyoMetro
+//
+//  Created by limc on 2014/09/25.
+//  Copyright (c) 2014年 Okasan-Huada. All rights reserved.
+//
+
+import Foundation
+
+extension String {
+    func blankIsNil() -> String{
+       return (self as NSString).blankIsNil();
+    }
+}
