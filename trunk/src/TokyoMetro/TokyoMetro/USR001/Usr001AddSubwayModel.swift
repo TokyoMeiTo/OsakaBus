121101d4ed16d653b38ba31f2a2617fe518a12ce
CaoWJ <caowj@ohs-sys.com>
2014-10-22 21:21:10 +0800

    代码优化
    代码优化

---
+++ trunk/src/TokyoMetro/TokyoMetro/USR001/Usr001AddSubwayModel.swift
@@ -0,0 +1,39 @@
+//
+//  Usr001AddSubwayModel.swift
+//  TokyoMetro
+//
+//  Created by caowj on 14-10-22.
+//  Copyright (c) 2014年 Okasan-Huada. All rights reserved.
+//
+
+import Foundation
+
+
+class Usr001AddSubwayModel {
+    
+    // 删除站点
+//    func removeSubway(ruteRowId: String) -> Bool {
+//        var table = UsrT03FavoriteTable()
+//        var map: UsrT03FavoriteTable = stationArr[index] as UsrT03FavoriteTable
+//        table.rowid = map.rowid
+//        
+//        return table.delete()
+//    }
+//    
+//    // 删除路线
+//    func removeRute(ruteRowId: String) -> Bool {
+//        var table = UsrT03FavoriteTable()
+//        table.rowid = ruteRowId
+//        
+//        return table.delete()
+//    }
+//    
+//    // 删除地标
+//    func removeLandMark(ruteRowId: String) -> Bool {
+//        var table = UsrT03FavoriteTable()
+//        var key = landMarkRwoIdArr[index] as UsrT03FavoriteTable
+//        table.rowid = key.rowid
+//        
+//        return table.delete()
+//    }
+}
